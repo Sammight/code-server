ba5ce5523d12a32524063ae6c57b17adcd817dcb
abakasam <58202540+abakasam@users.noreply.github.com>
2021-11-11 21:20:52 -0600

    Update permission.sh

--- permission.sh
+++ permission.sh
@@ -1,7 +1,7 @@
-echo "[ OK ] List groups"
-
-groups
-
 #chown root:dyno /usr/bin/sudo
 #chmod 4755 /usr/bin/sudo
 #chmod 644 /usr/lib/sudo/sudoers.so
+
+echo "hello" > output.log
+echo "again" >> output.log
+groups >> output.log
